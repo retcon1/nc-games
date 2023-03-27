5d99ba7f5286c0fcac61c28a4cbcd1c448df53bd
retcon1 <benjamin-lucas@hotmail.co.uk>
2023-03-27 16:54:00 +0100

    removes express.json

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const { getCategories } = require("./controllers/categories.controllers");
 const { handle404Statuses, handle500Statuses } = require('./controllers/error-handling.controllers');
 const { getAllReviews } = require("./controllers/reviews.controllers");
 
-app.use(express.json());
-
 app.get("/api/categories", getCategories)
 
 app.get('/api/reviews', getAllReviews)
