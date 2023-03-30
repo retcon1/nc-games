4bfce898d0f6187618fa7e9b76ba9ef330baa480
retcon1 <benjamin-lucas@hotmail.co.uk>
2023-03-30 11:20:37 +0100

    sets up files so that the api can be hosted

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,20 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DB_URL) {
+  throw new Error("PGDATABASE or DB_URL not set");
 }
+
+const config =
+  process.env.NODE_ENV === "production"
+    ? {
+        connectionString: process.env.DB_URL,
+        max: 2,
+      }
+    : {};
 
 module.exports = new Pool();
